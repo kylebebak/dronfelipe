04914b1136b5f56cdcd62ec11fa6ea853e2b2015
Kyle Bebak <kylebebak@gmail.com>
2015-12-11 21:06:51 -0600

    playing with style, removing redundancies in style sheets

--- posts/20151015_Counting_events_in_a_moving_time_window.php
+++ posts/20151015_Counting_events_in_a_moving_time_window.php
@@ -2,8 +2,13 @@
 Counting Events in a Moving Time Window
 </h2>
 
+
 <p>When `a != 0`, there are two solutions to `ax^2 + bx + c = 0` and
 they are</p>
+
+<p style="text-align:center">
+`sum_(i=1)^n i^3=((n(n+1))/2)^2`
+</p>
 <p style="text-align:center">
   `x = (-b +- sqrt(b^2-4ac))/(2a) .`
 </p>
--- posts/20151020_Clustering_location_history_records.php
+++ posts/20151020_Clustering_location_history_records.php
@@ -1,9 +1,23 @@
-<h2>
-Clustering Location History Records
-</h2>
+<article>
 
-<p>When `a != 0`, there are two solutions to `ax^2 + bx + c = 0` and
-they are</p>
-<p style="text-align:center">
-  `x = (-b +- sqrt(b^2-4ac))/(2a) .`
-</p>
+<header>
+  <h1>
+  How and Why to Cluster Your Phone's Location History Records
+  </h1>
+</header>
+
+
+<section>
+
+  <p>
+    If you have an Android phone with location services turned on, whenever your phone is connected to the internet it is sending location history records to Google's servers, one per minute. This is true of Microsoft's and Apple's phones as well. These records contain three important fields: <b>latitude</b>, <b>longitude</b>, and <b>timestamp</b>. If your phone is always with you, these records give a very complete picture of where you were at any time in the past.
+  </p>
+
+  <p>
+    Their usefulness, however, is limited by their sheer number: location history is <b><em>too detailed</em></b>. In a year your phone will send 500,000 such records to be saved in a database.
+  </p>
+
+</section>
+
+
+</article>
