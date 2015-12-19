6e031668e904940bd0453d2a3af971cc0afd7f90
Kyle Bebak <kylebebak@gmail.com>
2015-12-19 09:41:13 -0600

    fixed typos in counting events post

--- posts/counting_events_in_a_moving_window.php
+++ posts/counting_events_in_a_moving_window.php
@@ -8,7 +8,7 @@
 
 <h3>Detecting Anomalies</h3>
 <p>
-  Imagine you're writing a program that reads a series of transactions in real time, and raises an alert whenever more than 50 occur in an hour. Or whenever the total value of these transactions exceeds 500 dollars. Full disclosure: I did this for a big client in Mexico that wanted to detect fraudulent transactions in their stores, both for issuing real-time alerts and for reviewing historical data. I'm not sure whether stores in the US worry about this, but in any case this sort of problem gives rise to an interesting programming model called <a href="https://en.wikipedia.org/wiki/Discrete_event_simulation">discrete event simulation</a>.
+  Imagine you're writing a program that reads a series of transactions in real time, and raises an alert whenever more than 50 occur in an hour. Or whenever the total value of these transactions exceeds 500 dollars. Full disclosure: I did this for a big client in Mexico that wanted to detect fraudulent transactions in their stores, both for issuing real-time alerts and for reviewing historical data. I'm not sure whether stores in the U.S. worry about this, but in any case this sort of problem gives rise to an interesting programming model called <a href="https://en.wikipedia.org/wiki/Discrete_event_simulation">discrete event simulation</a>.
 </p>
 
 <p>
@@ -25,7 +25,7 @@
 </p>
 
 <p>
-  But this inefficient solution leads to an insight. <b>Namely, we need an hour-long <em>"window"</em> through which we can examine blocks of consecutive events.</b> But instead of moving the window forward, event by event, we can stream the events through the window. This allows to see when the threshold is exceeded, but issues <b>just one query</b> to the database. It turns out a queue is the ideal structure for this "stationary" window.
+  But this inefficient solution leads to an insight. <b>Namely, we need an hour-long <em>"window"</em> through which we can examine blocks of consecutive events.</b> But instead of moving the window forward, event by event, we can stream the events through the window. This allows us to see when the threshold is exceeded, but issues <b>just one query</b> to the database. It turns out a queue is the ideal structure for this "stationary" window.
 </p>
 
 <h3>Using the Queue</h3>
@@ -39,8 +39,7 @@ flagged_events <span style="color: #333333">=</span> <span style="color: #007020
 
 <span style="color: #008800; font-weight: bold">for</span> event <span style="color: #000000; font-weight: bold">in</span> events:
     q<span style="color: #333333">.</span>enqueue(event) <span style="color: #888888"># newest event is on back of queue</span>
-    back <span style="color: #333333">=</span> q<span style="color: #333333">.</span>back()
-    <span style="color: #008800; font-weight: bold">while</span> back<span style="color: #333333">.</span>time() <span style="color: #333333">-</span> q<span style="color: #333333">.</span>front()<span style="color: #333333">.</span>time() <span style="color: #333333">&gt;</span> interval:
+    <span style="color: #008800; font-weight: bold">while</span> q<span style="color: #333333">.</span>back()<span style="color: #333333">.</span>time() <span style="color: #333333">-</span> q<span style="color: #333333">.</span>front()<span style="color: #333333">.</span>time() <span style="color: #333333">&gt;</span> interval:
         q<span style="color: #333333">.</span>dequeue()
     <span style="color: #008800; font-weight: bold">if</span> q<span style="color: #333333">.</span>length() <span style="color: #333333">&gt;</span> threshold:
         flagged_events<span style="color: #333333">.</span>append(event)
@@ -53,6 +52,6 @@ flagged_events <span style="color: #333333">=</span> <span style="color: #007020
 </p>
 
 <p>
-  On average then, for every event that goes on the queue, one comes off. So, for an <b>average iteration</b> of the algorithm, we look at <b>three events</b>: the new one that goes on the back of the queue, the oldest one at the front of the queue (which usually gets dequeued), and the second oldest one (which usually doesn't get dequeued). We are reading <code>3N</code> events and making <code>2N</code> comparisons to process <code>N</code> events, which means our approach is <b>linear</b> in the number of events!
+  On average then, for every event that goes on the queue, one comes off. So, for an <b>average iteration</b> of the algorithm, we look at <b>three events</b>: the new one that goes on the back of the queue, the oldest one at the front of the queue (which usually gets dequeued), and the second oldest one (which usually doesn't get dequeued). We are reading <code>3N</code> events and making <code>2N</code> comparisons to process <code>N</code> events, which means our approach is <b>linear</b> in the number of events.
 </p>
 
